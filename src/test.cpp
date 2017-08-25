5f2b819416c2647aace0fa7cea54e91cd52de4da
mDenissk <denissk@metropolia.fi>
2017-08-25 11:30:37 +0300

    test commit

---
+++ src/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Aug 25, 2017
+ *      Author: komar
+ */
+
+#include "test.h"
+
+test::test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+test::~test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/test.h
@@ -0,0 +1,17 @@
+/*
+ * test.h
+ *
+ *  Created on: Aug 25, 2017
+ *      Author: komar
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+#endif /* TEST_H_ */
